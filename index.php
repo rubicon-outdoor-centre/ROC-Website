e88d267fa5fa8aa13b93b6e397e4dabcb89048d4
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-07 16:22:13 +1100

    add footer.php

--- index.php
+++ index.php
@@ -40,22 +40,4 @@
                 </section>
             </div>
         </div>
-        <footer class="footer">
-            <p class="copyright">&copy; 2012 Rubicon Outdoor Centre</p>
-            <nav class="footer__nav">
-                <ul class="footer__nav__list">
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor  footer__nav__anchor--current" href="/index.php">Home</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/teachers.php">Teachers</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/students.php">Students</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/media.php">Media</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/venue_hire.php">Venue Hire</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/activities.php">Activities</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/about.php">About</a></li>
-                    <li class="footer__nav__list-item"><a class="footer__nav__anchor" href="/contact.php">Contact</a></li>
-                </ul>
-            </nav>
-        </footer>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script src="/js/main.js"></script>
-    </body>
-</html>
+<?php include("/includes/footer.php"); ?>
