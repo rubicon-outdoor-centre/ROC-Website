c074dcddd5d18f45ef7cf8e681762d890cf44005
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-08 12:01:11 +1100

    fix links in nayook/includes/header.php

--- nayook/includes/header.php
+++ nayook/includes/header.php
@@ -6,18 +6,18 @@
                 </ul>
             </nav>
             <hgroup class="site-title">
-                <h1 class="site-title__main"><a class="site-title__anchor" href="/index.php">Rubicon Outdoor Centre</a></h1>
+                <h1 class="site-title__main"><a class="site-title__anchor" href="/nayook/index.php">Rubicon Outdoor Centre</a></h1>
                 <h2 class="site-title__sub">Nayook Campus</h2>
             </hgroup>
             <nav class="main-nav">
                 <ul class="main-nav__list">
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--home" href="/index.php">Home</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--teachers" href="/teachers.php">Teachers</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--students" href="/students.php">Students</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--media" href="/media.php">Media</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--activities" href="/activities.php">Activities</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--about" href="/about.php">About</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--contact" href="/contact.php">Contact</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--home" href="/nayook/index.php">Home</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--teachers" href="/nayook/teachers.php">Teachers</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--students" href="/nayook/students.php">Students</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--media" href="/nayook/media.php">Media</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--activities" href="/nayook/activities.php">Activities</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--about" href="/nayook/about.php">About</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--contact" href="/nayook/contact.php">Contact</a></li>
                 </ul>
             </nav>
             <img class="main-banner" src="http://valleyviewhomestay.in/embedded-slideshow/images/1.jpg" alt="Banner Image">
