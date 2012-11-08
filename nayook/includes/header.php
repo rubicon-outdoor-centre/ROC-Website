769d4af54bce6bb6755d7c2f99f51bdea3cd5b30
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-08 11:40:04 +1100

    change nayook campus header to use an include

---
+++ nayook/includes/header.php
@@ -0,0 +1,24 @@
+		<header>
+            <nav class="campus-select">
+                <ul class="campus-select__list">
+                    <li class="campus-select__list-item"><a class="campus-select__anchor  campus-select__anchor--current" href="/nayook/index.php">Nayook Campus</a></li>
+                    <li class="campus-select__list-item"><a class="campus-select__anchor" href="/index.php">Rubicon Campus</a></li>
+                </ul>
+            </nav>
+            <hgroup class="site-title">
+                <h1 class="site-title__main"><a class="site-title__anchor" href="/index.php">Rubicon Outdoor Centre</a></h1>
+                <h2 class="site-title__sub">Nayook Campus</h2>
+            </hgroup>
+            <nav class="main-nav">
+                <ul class="main-nav__list">
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--home" href="/index.php">Home</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--teachers" href="/teachers.php">Teachers</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--students" href="/students.php">Students</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--media" href="/media.php">Media</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--activities" href="/activities.php">Activities</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--about" href="/about.php">About</a></li>
+                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--contact" href="/contact.php">Contact</a></li>
+                </ul>
+            </nav>
+            <img class="main-banner" src="http://valleyviewhomestay.in/embedded-slideshow/images/1.jpg" alt="Banner Image">
+        </header>
