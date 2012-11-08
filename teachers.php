94c7a06843bf0eca535851de1776124cb16a8ccb
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-08 11:35:38 +1100

    make main-nav an include

--- teachers.php
+++ teachers.php
@@ -2,31 +2,7 @@
         <title>Rubicon Outdoor Centre - Teachers</title>
     </head>
     <body id="top" class="page--teachers">
-        <header>
-            <nav class="campus-select">
-                <ul class="campus-select__list">
-                    <li class="campus-select__list-item"><a class="campus-select__anchor" href="/nayook/index.php">Nayook Campus</a></li>
-                    <li class="campus-select__list-item"><a class="campus-select__anchor  campus-select__anchor--current" href="/index.php">Rubicon Campus</a></li>
-                </ul>
-            </nav>
-            <hgroup class="site-title">
-                <h1 class="site-title__main"><a class="site-title__anchor" href="/index.php">Rubicon Outdoor Centre</a></h1>
-                <h2 class="site-title__sub">Rubicon Campus</h2>
-            </hgroup>
-            <nav class="main-nav">
-                <ul class="main-nav__list">
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/index.php">Home</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor  main-nav__anchor--current" href="/teachers.php">Teachers</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/students.php">Students</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/media.php">Media</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/venue_hire.php">Venue Hire</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/activities.php">Activities</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/about.php">About</a></li>
-                    <li class="main-nav__list-item"><a class="main-nav__anchor" href="/contact.php">Contact</a></li>
-                </ul>
-            </nav>
-            <img class="main-banner" src="http://www.intangibility.com/Photography/Collections/GP/GP_126-Wyoming-5/GP_126_11-13-%5Bweb%5D.jpg" alt="Banner Image">
-        </header>
+        <?php include("/includes/header.php"); ?>
         <div class="main-content  grid">
             <div class="page-links  grid__col--33">
                 <h3 class="page-links__title">Page Sections</h3>
