7aa655f72228692a7a93527f3de283f6990206f5
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-09 12:08:19 +1100

    add back to top link to footer

--- includes/footer.php
+++ includes/footer.php
@@ -10,6 +10,7 @@
                     <li class="footer__nav__list-item"><a class="footer__nav__anchor  footer__nav__anchor--activities" href="/activities.php">Activities</a></li>
                     <li class="footer__nav__list-item"><a class="footer__nav__anchor  footer__nav__anchor--about" href="/about.php">About</a></li>
                     <li class="footer__nav__list-item"><a class="footer__nav__anchor  footer__nav__anchor--contact" href="/contact.php">Contact</a></li>
+                    <li class="footer__nav__list-item"><a class="footer__nav__anchor  footer__nav__anchor--contact" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a></li>
                 </ul>
             </nav>
         </footer>
