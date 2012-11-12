26115f3804b828d7aa09167305cca406a12daf2a
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-12 13:26:41 +1100

    improve image layout on activities.php

--- activities.php
+++ activities.php
@@ -25,67 +25,53 @@
                     <h2 class="section__title">Bush Walking</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>Bushwalks may be conducted in the Rubicon, Royston or Cathedral Ranges.</p>
-                    <p>Overnight Campouts may be negotiated as a part of a two or three day bushwalk. Students participating in an Overnight campout must consult the <a href="downloads/student%20equipment%20list.pdf" target="_blank">Student Equipment List</a>.</p>
-                    <img src="/images/rubicon-activities/bushwalking1.jpg" alt="bushwalking1"/>
-                    <img src="/images/rubicon-activities/bushwalking2.jpg" alt="bushwalking2"/>
+                    <p>Overnight Campouts may be negotiated as a part of a two or three day bushwalk. Students participating in an Overnight campout must consult the <a href="#" target="_blank">Student Equipment List</a>.</p>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="canoeing" class="section">
                     <h2 class="section__title">Canoeing</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>The Rubicon Campus is ideally situated for all kinds of canoeing activities. Lake Eildon close by provides excellent flat water paddling, and the Goulburn River offers river touring and whitewater during the summer. For the autumn and spring, Big River provides a more challenging whitewater experience.</p>
                     <p>School programs normally involve a 3 hour flat water session to introduce students to the basic skills of canoeing. The program may then have a full day option of paddling on either the Goulburn River or Lake Eildon.</p>
-                    <img src="/images/rubicon-activities/canoeing2.jpg" alt="canoeing2"/>
-                    <img src="/images/rubicon-activities/canoeing1.jpg" alt="canoeing1"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="cross-country_skiing" class="section">
                     <h2 class="section__title">Cross-Country Skiing</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>Lake Mountain, Victoria's premier cross-country ski resort, is one hour by bus from Rubicon; as soon as it snows, we ski. We have a wide range of skiing equipment for students as well as quality rainwear to keep warm and dry.</p>
                     <p>Winter programs have the option of 2 full days of skiing (subject to snow conditions) at Lake Mountain. Students are introduced to the Alpine environment on cross country skis. In the event of poor snow conditions, alternative activites are provided.</p>
                     <p>During the day students are videoed in action. This video forms the basis of the evening activity with some reflection on their day.</p>
-                    <img src="/images/rubicon-activities/skiing2.jpg" alt="skiing2"/>
-                    <img src="/images/rubicon-activities/skiing1.jpg" alt="skiing1"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="environmental_studies" class="section">
                     <h2 class="section__title">Environmental Studies</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>Interaction with the natural environment through the outdoor activities we offer, is an integral part of Rubicon programs. Journey-based activities such as bush walking, rafting, canoeing, mountain bike riding, cross country skiing and exploration days provide many opportunities for environmental education. </p>
-                    <img src="/images/rubicon-activities/enviro1.jpg" alt="enviro1"/>
-                    <img src="/images/rubicon-activities/enviro2.jpg" alt="enviro2"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="expeditions" class="section">
                     <h2 class="section__title">Expeditions</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>The Rubicon Campus can provide equipment and expert staffing for specialised and extended programs.</p>
                     <p>We can arrange trips, activities and instruction anywhere in Victoria. Program development aligned to specific projects can be negotiated with a wide variety of options available.</p>
-                    <img src="/images/rubicon-activities/exp1.jpg" alt="exp1"/>
-                    <img src="/images/rubicon-activities/exp2.jpg" alt="exp2"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="high_ropes" class="section">
                     <h2 class="section__title">High Ropes</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>Rubicon's 12 element high ropes course provides the perfect experience to develop trust, teamwork and personal challenge.</p>
                     <p>Students are provided with a safety briefing and practice on the ground before having the opportunity to complete a variety of elements between 5 and 8 metres high. A flying fox option adds to the excitement and fun of the session.</p>
                     <p>The 'Challenge by Choice' philosophy provides for a learning experience that can enable participants to set targets to achieve their goals.</p>
-                    <img src="/images/rubicon-activities/highropes2.jpg" alt="highropes2"/>
-                    <img src="/images/rubicon-activities/highropes1.jpg" alt="highropes1"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="horse_riding" class="section">
                     <h2 class="section__title">Horse Riding</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>Rubicon offers a 3 hour introductory session, covering the basic skills of horse riding. Students learn how to mount, dismount, turn and stop their horse and how to walk, trot and canter. The horses used are carefully matched to the students' riding ability to promote maximum enjoyment and learning.</p>
                     <p>All students, even experienced riders, can find an appropriate level of challenge and enjoyment.</p>
                     <p>Riding boots, helmets and saddlebags are provided. Clothing is advised to be loose fitting and comfortable. Gentlemen (and boys) are advised to NOT wear boxer shorts.</p>
-                    <img src="/images/rubicon-activities/horseriding2.jpg" alt="horseriding2"/>
-                    <img src="/images/rubicon-activities/horseriding1.jpg" alt="horseriding1"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="initiative_activities" class="section">
                     <h2 class="section__title">Initiative Activities</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
@@ -93,28 +79,22 @@
                     <p>If your school chooses the focus of Leadership, the sequence of initiative activites allows students to practise their leadership and evaluate other peers leadership styles.</p>
                     <p>Initiatives are an ideal activity for schools who wish to focus on teamwork. Students can witness how a team needs to operate to be effective and improve on these in future activities.</p>
                     <p>During the initiatives, issues of meeting the challenge are addressed as students will learn to identify their strengths and weakness which will assist them in overcoming the next challenge.</p>
-                    <img src="/images/rubicon-activities/initiatives1.jpg" alt="initiatives1"/>
-                    <img src="/images/rubicon-activities/initiatives2.jpg" alt="initiatives2"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="mountain_biking" class="section">
                     <h2 class="section__title">Mountain Biking</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>The area around Rubicon abounds with great opportunities for students to get their first taste of mountain biking. From winding dirt roads to single track, it's all here. Rubicon has a fleet of quality mountain bikes in sizes to suit the smallest to the largest students.</p>
                     <p>Participants are guaranteed to have a great time and develop skills to provide them with a better understanding of riding 'off road'.</p>
-                    <img src="/images/rubicon-activities/biking1.jpg" alt="biking1"/>
-                    <img src="/images/rubicon-activities/biking2.jpg" alt="biking2"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="rafting" class="section">
                     <h2 class="section__title">Rafting</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p> We have rafts and equipment for up to 24 participants and can offer a number of great activities in the local area. The Goulburn River offers a short white water section suitable for a 2-3 hour introductory session and the Big River allows a great day trip on whitewater (in season). Rafts or inflatable canoes are the options of craft.</p>
                     <p>You can also have a very relaxing float down the Goulburn River where participants can learn the basics of guiding a raft and everyone has the leisure to bird watch or watch the scenery. Both the Big and the Goulburn Rivers provide an excellent venue for environmental studies.</p>
-                    <img src="/images/rubicon-activities/rafting2.jpg" alt="rafting2"/>
-                    <img src="/images/rubicon-activities/rafting1.jpg" alt="rafting1"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="rock_climbing" class="section">
                     <h2 class="section__title">Rock Climbing</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
@@ -122,16 +102,13 @@
                     <p>Climbing helps to foster a sense of confidence, trust, interdependence and group unity and encourages goal setting and persistence. All students, even experienced climbers, can find a challenge.</p>
                     <p>We also conduct full day climbing sessions in the Cathedral Ranges where students can enjoy climbing and abseiling on real cliffs amongst spectacular scenery. With a little experience, students may have the opportunity to tackle up to 60m climbs and abseils.</p>
                     <p>Climbing expeditions to Mt Arapiles or the Grampians are also possible.</p>
-                    <img src="/images/rubicon-activities/rockclimbing1.jpg" alt="rockclimbing1"/>
-                    <img src="/images/rubicon-activities/rockclimbing2.jpg" alt="rockclimbing2"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
-
                 <section id="rogaining" class="section">
                     <h2 class="section__title">Rogaining</h2>
                     <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
                     <p>Rogaining is the sport of cross-country navigation; teams of two to five people visit as many checkpoints as they can in the time allowed. Traditionally, Rogaining is a 24 hour event, however, at Rubicon we typically run a 1 &frac12; to 2 &frac12; hour session either in bushland, farmland or around the Rubicon camp site. It is a great opportunity for students to challenge their navigation skills and learn how to find their way around using a map.</p>
-                    <img src="/images/rubicon-activities/rogaining1.jpg" alt="rogaining1"/>
-                    <img src="/images/rubicon-activities/rogaining2.jpg" alt="rogaining2"/>
+                    <img class="activity-image" src="/images/rubicon-activities/foo.jpg" alt="foo"/>
                 </section>
             </div>
         </div>
