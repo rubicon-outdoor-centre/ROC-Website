fdcb4d3034ebab5d406f52676285025163a42eca
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-11-12 14:14:35 +1100

    add venue_hire.php

---
+++ venue_hire.php
@@ -0,0 +1,207 @@
+<?php include("/includes/head.php"); ?>
+        <title>Rubicon Outdoor Centre - Rubicon Campus - Media</title>
+    </head>
+    <body id="top" class="page--venue-hire">
+        <?php include("/includes/header.php"); ?>
+        <div class="main-content  grid">
+            <div class="page-links  grid__col--33">
+                <ul class="page-links__list">
+                    <li><a class="page-links__anchor" href="#make_a_booking">Make a Booking</a></li>
+                    <li><a class="page-links__anchor" href="#royston_house">Royston House</a></li>
+                    <li><a class="page-links__anchor" href="#local_attractions">Local Attractions</a></li>
+                </ul>
+            </div>
+            <div class="grid__col--66">
+                <section id="make_a_booking" class="section">
+                    <h2 class="section__title">Make a Booking</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>Rubicon Outdoor Centre is operated by the Department of Education and Early Childhood Development. It is available for short-term hire to groups or individuals seeking a venue for outdoor recreation, environmental education, and relaxation! The Centre can also be hired on a daily basis, providing an ideal setting for meetings and seminars.</p>
+                    <p>The Rubicon Campus is located in North-Eastern Victoria approximately 130 kilometres northeast of Melbourne in the Rubicon Valley, at the foot of the Blue Range providing a base for a range of activities. The Centre is situated approximately 18 kilometres - 15 minutes travel by road from both Alexandra and Eildon.</p>
+                    <p>Rubicon has varied dormitory style accommodation facilities.</p>
+                    <p>&ldquo;Goulburn&rdquo; and &ldquo;Torbreck&rdquo; dormitories each accommodate up to 22 persons with 2 persons per room. 10 rooms have a double bunk bed and the other room has 2 single beds with an ensuite. Each of these dormitories has 4 showers and handbasins and 1 indoor toilet.</p>
+                    <p>&ldquo;Pinninger&rdquo; accommodates up to 12 persons with 2 persons per room. 5 rooms have a double bunk bed and the other room has 2 single beds. This dormitory has 2 separate bathrooms with 1 toilet, 1 shower and 2 handbasins in each.</p>
+                    <p>Catering for Goulburn, Torbreck & Pinninger dormitories occurs in our newly refurbished commercial kitchen and dining room.</p>
+                    <p>Use of Commercial Kitchen Facilities to be negotiated. A Bond may be required.</p>
+                    <p>&ldquo;Sugarloaf&rdquo; is self contained with a kitchen which allows for self catering within the dormitory. This dormitory accommodates up to 13 persons. There are 3 rooms which have two double bunks (4 persons) and 1 room with a single bed. This dormitory has one bathroom (disabled access) with a toilet, handbasin and a shower. The second bathroom has a shower and handbasin. There is also a second, separate indoor toilet.</p>
+                    <p>Exclusive use of the site may be negotiated. A Cleaning Fee of $60 per extra dormitory will apply.</p>
+                    <p>Linen Hire is available at $15.00 per set and must be ordered upon booking.</p>
+                    <p>View Accommodation Layout: <a href="downloads/accommodation%20layout.pdf" target="_blank">PDF</a></p>
+                    <h3>Accommodation (all costs inclde GST):</h3>
+                    <table class="table">
+                        <tr>
+                            <td>
+                                <p> <strong>Dormitory</strong></p>
+                            </td>
+                            <td>
+                                <p> <strong>Per Adult</strong></p>
+                            </td>
+                            <td>
+                                <p> <strong>Per Child</strong> <br>(5-15yrs)</p></td>
+                            <td>
+                                <p> <strong>Minimum Charge</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Goulburn or Torbreck </p>
+                            </td>
+                            <td>
+                                <p> $28</p>
+                            </td>
+                            <td>
+                                <p> $20</p>
+                            </td>
+                            <td>
+                                <p> $200</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Sugarloaf or Pinninger</p>
+                            </td>
+                            <td>
+                                <p> $28</p>
+                            </td>
+                            <td>
+                                <p> $20</p>
+                            </td>
+                            <td>
+                                <p> $60</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Royston Community House</p>
+                            </td>
+                            <td>
+                                <p> $28</p>
+                            </td>
+                            <td>
+                                <p> $20</p>
+                            </td>
+                            <td>
+                                <p> $60</p>
+                            </td>
+                        </tr>
+                    </table>
+                    <h3>Facilities Only Hire:</h3>
+                    <table class="table">
+                        <tr>
+                            <td>
+                                <p> <strong>Conference Room</strong> &nbsp;</p>
+                            </td>
+                            <td>
+                                <p> $40 per hr</p>
+                            </td>
+                            <td>
+                                <p> Minimum Charge $120</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p><strong>Dining Room &amp; Kitchen</strong> <br>Includes cooking facilities. Suitable for weddings, parties etc.</p>
+                            </td>
+                            <td>
+                                <p> $300</p>
+                             </td>
+                            <td>
+                                <p> Bond may be required</p>
+                            </td>
+                        </tr>
+                    </table>
+                    <h3>Activities (3 hour sessions):</h3>
+                    <table class="table">
+                        <tr>
+                            <td>
+                                <p> <strong>Canoeing</strong> <br>(12 years &amp; over) &nbsp;</p>
+                            </td>
+                            <td>
+                                <p> $60 Per Person (Minimum $420)</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p> <strong>Climbing</strong> <br>(8 years &amp; over) &nbsp;</p>
+                            </td>
+                            <td>
+                                <p> $60 Per Person (Minimum $420)</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p> <strong>Mountain Bikes</strong> <br>(12 years &amp; over) &nbsp;</p>
+                            </td>
+                            <td>
+                                <p> $60 Per Person (Minimum $420)</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p> <strong>Highropes</strong> <br>(8 years &amp; over) &nbsp;</p>
+                            </td>
+                            <td>
+                                <p> $60 Per Person (Minimum $420)</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p> <strong>Rafting</strong> <br>(12 years &amp; over) &nbsp;</p>
+                            </td>
+                            <td>
+                                <p> $100 Per Person (Minimum $600)</p>
+                            </td>
+                        </tr>
+                    </table>
+                    <p><strong>Catering can be negotiated.</strong></p>
+                    <p><strong>Disclaimer:</strong> All activities involve some degree of risk. Participants will be reqired to sign a waiver before participating.</p>
+                    <p>For bookings and further information contact:</p>
+                    <p><strong>Rubicon Campus</strong> Phone: (03) 5773 2285  Fax: (03) 5773 2441 <a href="mailto:rubicon.oc@edumail.vic.gov.au" data-tooltip="Email us using your email client">rubicon.oc@edumail.vic.gov.au</a></p>
+                    <p>View Booking Conditions: <a href="downloads/booking%20conditions.pdf" target="_blank">PDF</a></p>
+                    <p>Download Booking Form: <a href="downloads/booking%20form.pdf" target="_blank">PDF</a> <a href="downloads/booking%20form.xls">XLS</a></p>
+                    <p>All correspondence should be directed to:</p>
+                    <p><strong>Rubicon Outdoor Centre</strong> RMB 264 Rubicon Road Thornton VIC 3712</p>
+                </section>
+
+                <section id="royston_house" class="section">
+                    <h2 class="section__title">Royston House</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>Royston Community House is self contained and located on top of the Royston Range, 13 km further south of the Rubicon Campus.  It is available for hire in non fire danger and non winter periods.</p>
+                    <p>The kitchen has a small stove, pots and pans plus basic utensils, cutlery and crockery.  Bunk beds with mattresses are provided.</p>
+                    <p>BYO pillow, sleeping bag, towels, warm clothes and indoor shoes eg slippers.</p>
+                    <p>Gas wall heaters keep the rooms warm, whilst gas hot water is on tap in the kitchen.</p>
+                    <p>BYO esky for food storage in warmer months.</p>
+                    <p>A fire pit is located within the grounds.</p>
+                    <p>Power is supplied by a generator.</p>
+                    <p>Telephone reception is not available.</p>
+                    <p>View House Layout: <a href="downloads/royston%20house%20layout.pdf" target="_blank">PDF</a></p>
+                    <h3>Location:</h3>
+                    <p>The Rubicon Campus is located in the Rubicon Valley at the foot of the Blue Range, approximately 130 kms northeast of Melbourne. The Centre is situated approximately 18 kms &ndash; 15 minutes travel by road from both Alexandra and Eildon and is an ideal base for a range of activities.</p>
+                    <p>Royston Community House is a further 13 km south &mdash; specific instructions will be provided upon successful application.</p>
+                    <p>The road to Royston may be subject to snow at times and wheel chains might be required.</p>
+                    <p>Log trucks work in the area and great care needs to be taken on this single lane road.</p>
+                    <p>For bookings and further information contact:</p>
+                    <p><strong>Rubicon Campus</strong> Phone: (03) 5773 2285 Fax: (03) 5773 2441 <a href="mailto:rubicon.oc@edumail.vic.gov.au" data-tooltip="Email us using your email client">rubicon.oc@edumail.vic.gov.au</a></p>
+                    <p>Download Booking Form: <a href="downloads/booking%20form.pdf" target="_blank">PDF</a> <a href="downloads/booking%20form.xls">XLS</a></p>
+                    <p>All correspondence should be directed to:</p>
+                    <p><strong>Rubicon Outdoor Centre</strong> RMB 264 Rubicon Road Thornton VIC 3712</p>
+                </section>
+
+                <section id="local_attractions" class="section">
+                    <h2 class="section__title">Local Attractions</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>Only one and a half hours drive from Melbourne, through lush green valleys and forests of soaring Mountain Ash, Murrindindi is not so much a region as a lifestyle.</p>
+                    <p>Towns and villages nestle in a landscape of mountains, forests, lakes and sparkling streams; fertile farmlands supply an abundance of natural produce. It's a place to relax and unwind, indulge in treasured pastimes and seek out new adventures.</p>
+                    <p>Wander through magnificent forests to picnic beside fern gullies and cascading waterfalls, cast a line in search of fresh trout for dinner, or sample the mouth-watering delights of local berry farms, orchards and vineyards.</p>
+                    <p>Water-ski on the waters of Lake Eildon, take to the snow-covered slopes of Lake Mountain in winter or hike to the peaks of the Cathedral Range for breathtaking views over the surrounding countryside. Explore arts and culture at artist studios and craft galleries, and gain a glimpse into the past at fascinating historic attractions.</p>
+                    <p>It's easy to relax amid the tranquil beauty of Murrindindi.</p>
+                    <p>For further information on: Destinations/Townships, Attractions, Activities, Scenic Drives, What's New and the Calendar of Events go to the <a href="http://www.murrindindi.vic.gov.au" target="_blank">Murrindindi Shire site</a>.</p>
+                    <h3>Local Visitor Information Centres:</h3>
+                    <p><strong>Alexandra</strong> 36 Grant Street Alexandra, VIC, 3714 Ph: (03) 5772 1100 or 1800 652 298 <a href="mailto:alexvic@virtual.net.au" data-tooltip="Email using your email client">alexvic@virtual.net.au</a></p>
+                    <p><strong>Eildon</strong> Main Street Eildon, VIC, 3713 Ph: (03) 5774 2909 <a href="mailto:eildoninfo@lakeeildon.com" data-tooltip="Email using your email client">eildoninfo@lakeeildon.com</a></p>
+                    <p><strong>Kinglake</strong> 8 Parkland Road Kinglake, VIC, 3763 Ph: (03) 5786 1328 <a href="mailto:houseofbottles@tsis.com.au" data-tooltip="Email using your email client">houseofbottles@tsis.com.au</a></p>
+                    <p><strong>Marysville</strong> Darwin Street Marysville, VIC, 3779 Ph: 0429 000 394 <a href="mailto:info@marysvilletourism.com" data-tooltip="Email using your email client">info@marysvilletourism.com</a></p>
+                    <p><strong>Yea</strong> 20 High Street Yea, VIC, 3717 Ph: (03) 5797 2663 <a href="mailto:yvic@bigpond.com" data-tooltip="Email using your email client">yvic@bigpond.com</a></p>
+                </section>
+            </div>
+        </div>
+<?php include("/includes/footer.php"); ?>
