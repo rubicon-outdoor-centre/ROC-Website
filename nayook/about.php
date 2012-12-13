ca4f9cebeac4d910a361a3abdcaf2100e9652a92
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-12-13 11:16:08 +1100

    add Nayook page files

---
+++ nayook/about.php
@@ -0,0 +1,158 @@
+<?php include("../includes/head.php"); ?>
+        <title>Rubicon Outdoor Centre - Rubicon Campus - Media</title>
+    </head>
+    <body id="top" class="page--about">
+        <?php include("/includes/header.php"); ?>
+        <div class="main-content  grid">
+            <div class="page-links  grid__col--33">
+                <ul class="page-links__list">
+                    <li><a class="page-links__anchor" href="#school_profile">School Profile</a></li>
+                    <li><a class="page-links__anchor" href="#location">Location</a></li>
+                    <li><a class="page-links__anchor" href="#facilities">Facilities</a></li>
+                    <li><a class="page-links__anchor" href="#sustainability">Sustainability</a></li>
+                    <li><a class="page-links__anchor" href="#privacy_statement">Privacy Statement</a></li>
+                </ul>
+            </div>
+            <div class="grid__col--66">
+                <section id="school_profile" class="section">
+                    <h2 class="section__title">School Profile</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>Rubicon  Outdoor Centre is a residential Outdoor Education school operated by the DEECD.  Rubicon has two campuses; the original Rubicon Campus is in the picturesque  Rubicon Valley approximately 100km north-east of Melbourne. The Nayook Campus,  located near Neerim Junction, approximately 100 km east of Melbourne, is a  recent addition to the school. The integration of this facility into the  Rubicon programs is progressing and presents exciting opportunities for the  school.</p>
+                    <p>Rubicon caters for more than 3000 students annually.</p>
+                    <p>The combined staffing of both campuses is made up of 3 Principal Class, 10 teaching staff, 6  ES staff, 2 cooks and various cleaning &amp; grounds staff. The school makes  opportunities available for Tertiary students doing Outdoor Education Courses,  ES Trainees and Work Experience Students to have on-site professional learning.</p>
+                    <p>The grounds and buildings, including administration, kitchen, dormitories and other external facilities, are extremely well maintained. The Nayook site has recently undergone extensive building refurbishment with further development planned.</p>
+                    <p>The school offers programs for schools under the following categories:</p>
+                        <ol>
+                            <li><p>Years 7-10; VELS levels 4, 5 &amp; 6; Domains Outdoor Education, and Personal Development</p></li>
+                            <li><p>VCE Outdoor and Environmental Studies</p></li>
+                            <li><p>The VCAL program is specifically focused on personal development skills, teamwork, challenge, and leadership</p></li>
+                            <li><p>Students with Special Needs attend programs negotiated with schools that are designed to meet individual needs</p></li>
+                        </ol>
+                    <p>Activities to compliment these programs include bush walking, canoeing (flat or moving water), caving, cross-country skiing, environmental studies, expeditions, high ropes, horse riding, initiative activities, mountain biking, rafting, rock climbing and rogaining.</p>
+                    <p>The environment is used as a medium for building leadership, personal development and teamwork, through residential, single-day and off-campus programs. Environmental sustainability is a very strong focus of all programs offered.</p>
+                </section>
+
+                <section id="location" class="section">
+                    <h2 class="section__title">Location</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>The Rubicon Campus is located at 264 Rubicon Road, Thornton, approximately 130 km from Melbourne, not far off the Maroondah Highway enroute to Eildon.</p>
+                    <p>Travel directions differ dependant upon where you are travelling from.</p>
+                    <p><strong>Southern and Eastern suburbs of Melbourne:</strong></p>
+                    <ol>
+                        <li><p>Follow Maroondah Highway north through Lilydale and Healesville, over the Black Spur.</p></li>
+                        <li><p>Turn right at Taggerty, towards Eildon. </p></li>
+                        <li><p>Approximately 9km from Taggerty look for the Rubicon Road turnoff on the right, (signposted &lsquo;Rubicon Outdoor Centre&rsquo;) just before the Rubicon River Bridge.</p></li>
+                        <li><p>The Rubicon Campus is approximately 2.5km on the right.</p></li>
+                        <li><p>Please use the entrance just past the oval.</p></li>
+                    </ol>
+                    <p><strong>Northern and Western suburbs of Melbourne, Central or Northern Victorian visitors may find it easier to travel through Yea:</strong></p>
+                    <ol>
+                        <li><p>From Yea, follow signs to Alexandra. At Alexandra, turn left towards Eildon.</p></li>
+                        <li><p>At Thornton turn right and in 2km look for the Rubicon Road turnoff on the left, (signposted &lsquo;Rubicon Outdoor Centre&rsquo;) just after the Rubicon River Bridge.</p></li>
+                        <li><p>The Rubicon Campus is approximately 2.5km on the right.</p></li>
+                        <li><p>Please use the entrance just past the oval.</p></li>
+                    </ol>
+                    <div id="map_canvas" class="frame"></div>
+                    <p><a href="http://goo.gl/maps/R792" target="_blank">View using Google Maps</a></p>
+                </section>
+
+                <section id="facilities" class="section">
+                    <h2 class="section__title">Facilities</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>Rubicon has varied accommodation facilities including dormitories, two self-contained units, and Royston House, a hostel high in the Rubicon Ranges. Specialist teaching equipment e.g. skis, rafts, tents and canoes are supplied and internal transport is provided by buses and 4WD vehicles.</p>
+                    <p>The Rubicon Campus has a well-equipped laundry, drying room and clothes line for students. The laundry has two washing machines, and a tumble dryer. There is a phone available for the use of visiting teachers and, under certain conditions, students.</p>
+                    <p>A bouldering wall, basketball/netball court, beach volleyball court, playing oval, a variety of sports equipment and recreation hall complete with table tennis, pool table and foos ball are available for use at specified times. Students may bring their own musical instruments, games, cards and CD's. A CD player is provided in the Hall for students to use. iPods and other personal listening devices should not be brought to the Centre.</p>
+                </section>
+
+                <section id="sustainability" class="section">
+                    <h2 class="section__title">Sustainability</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <h3>Solar Power Production:</h3>
+                    <p>Rubicon has a 2 kW photovoltaic power system that has been producing since the 17th of May 2008.</p>
+                    <p><strong>Yearly power production:</strong></p>
+                    <p>
+                        <ul>
+                            <li>2008 &ndash; 1654 kWh</li>
+                            <li>2009 &ndash; 2777 kWh</li>
+                            <li>2010 &ndash; 2693 kWh</li>
+                            <li>2011 &ndash; 2671 kWh</li>
+                        </ul>
+                    </p>
+                    <p><strong>Monthly power production for 2012:</strong></p>
+                    <p>
+                        <ul>
+                            <li>January &ndash; 322 kWh</li>
+                            <li>February &ndash; 260 kWh</li>
+                            <li>March &ndash; 252 kWh</li>
+                            <li>April &ndash; 209 kWh</li>
+                            <li>May &ndash; 137 kWh</li>
+                            <li>June &ndash; 194 kWh</li>
+                            <li>July &ndash; 98 kWh</li>
+                            <li>August &ndash; 150 kWh</li>
+                            <li>September &ndash; 211 kWh</li>
+                        </ul>
+                    </p>
+                    <p><strong>Monthly power production for 2011:</strong></p>
+                    <p>
+                        <ul>
+                            <li>January &ndash; 321 kWh</li>
+                            <li>February &ndash; 238 kWh</li>
+                            <li>March &ndash; 271 kWh</li>
+                            <li>April &ndash; 222 kWh</li>
+                            <li>May &ndash; 141 kWh</li>
+                            <li>June &ndash; 107 kWh</li>
+                            <li>July &ndash; 109 kWh</li>
+                            <li>August &ndash; 183 kWh</li>
+                            <li>September &ndash; 190 kWh</li>
+                            <li>October &ndash; 275 kWh</li>
+                            <li>November &ndash; 263 kWh</li>
+                            <li>December &ndash; 351 kWh</li>
+                        </ul>
+                    </p>
+                    <img src="images/sustainability/solar-panels.jpg" alt="Solar Panels"/>
+                    <p><strong>Recycling Case Study 2012:</strong> <a href="downloads/recycling-case-study-2012.pdf" target="_blank">PDF</a></p>
+                    <h3>Green Purchasing Initiatives in 2011:</h3>
+                    <p>
+                        <ul>
+                            <li><p>Purchased a new worm farm from CERES constructed from a reused wheelie bin.</p></li>
+                            <li><p>Started replacing our cheap over pants with One Planet Goretex pants which will last much longer and be more suitable for the conditions we use them in.</p></li>
+                            <li><p>Energy efficient photocopier with double sided capacity.</p></li>
+                        </ul>
+                    </p>
+                    <h3>Other Sustainability Initiatives in 2011:</h3>
+                    <p>
+                        <ul>
+                            <li><p>Motion sensors in student bathrooms.</p></li>
+                            <li><p>New veggie patch.</p></li>
+                            <li><p>Worm farm castings and liquid is used to fertilize veggie patch.</p></li>
+                            <li><p>New double glazed door on student drying room resulting in greatly improved heat retention.</p></li>
+                            <li><p>Old bike parts and bike helmets reused and recycled with <a href="http://www.theage.com.au/news/national/recycling/2007/10/30/1193618878006.html">Bill the bike man</a> at human powered cycles.</p></li>
+                            <li><p>Old daypacks and coats donated to the <a href="http://www.asrc.org.au">Asylum Seeker Resource Centre</a>.</p></li>
+                            <li><p>Obsolete ICT equipment sent to <a href="http://cashforcomputers.com.au">Cash 4 Computers</a> to be reused in developing world environments and non-functioning machines pulled apart and metals salvaged.</p></li>
+                            <li><p>Water capturing water cans for student showers (water then used on veggie garden).</p></li>
+                            <li><p>Food scraps going to feed animals rather than land fill.</p></li>
+                        </ul>
+                    </p>
+                    <img src="images/sustainability/veggie-garden.jpg" alt="Veggie Garden"/>
+                    <br>
+                    <img src="images/sustainability/food-scraps.jpg" alt="Food Scraps"/>
+                </section>
+
+                <section id="privacy_statement" class="section">
+                    <h2 class="section__title">Privacy Statement</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <p>Rubicon collects personal information eg. Medical, Consent, and Horse riding Permission forms, for the purpose of providing the best possible care for your child. We respect the rights of students, staff and parents to privacy of this information.</p>
+                    <p>All Rubicon staff and the Department of Education and Early Childhood Development (DEECD) are required by law to protect this information provided by you.</p>
+                    <p>Health information is asked for such that Rubicon staff can better care for your child. This information includes any medical condition or disability your child may have; medication your child may require whilst at camp; any known allergies and contact details of your child's doctor. Rubicon depends on you to provide all relevant health information; withholding health information may put your child's health and safety at risk.</p>
+                    <p>Information concerning parents, guardians or carers, allows us to take account of family arrangements. Family Court Orders outlining any access restrictions and parenting plans should be made available to Rubicon Outdoor Centre. Any changes to these arrangements must be communicated to Rubicon as soon as possible.</p>
+                    <p>Please do not hesitate to contact the Principal if you would like to discuss in confidence, any matters relating to family arrangements.</p>
+                    <h3>Emergency Contacts:</h3>
+                    <p>These are people that Rubicon Outdoor Centre may contact in an emergency. Please ensure people named are aware that they have been nominated as emergency contacts and agree to their details being provided to Rubicon.</p>
+                    <p>Download Privacy Policy: <a href="downloads/privacy%20policy.pdf" target="_blank">PDF</a></p>
+                    <p>If you have any concerns about the confidentiality of this information please contact the Principal.</p>
+                    <p>Rubicon Outdoor Centre is a school, and as such is governed by the Department of Education and Early Childhood Development (DEECD). The DEECD Information Privacy Policy is available <a href="http://www.education.vic.gov.au/privacy.htm" target="_blank">here</a>.</p>
+                    <p><em>If you have trouble viewing these files please download the latest version of <a href="http://www.adobe.com/products/acrobat/readstep2.html" target="_blank">Acrobat Reader</a>.</em></p>
+                </section>
+            </div>
+        </div>
+<?php include("/includes/footer.php"); ?>
