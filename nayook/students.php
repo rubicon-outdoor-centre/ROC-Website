1b00df90c2ac78613fd85a4ebb0e62e45b01461d
Joshua Hibbert <joshuanhibbert@gmail.com>
2012-12-13 11:16:08 +1100

    add Nayook page files

---
+++ nayook/students.php
@@ -0,0 +1,120 @@
+<?php include("../includes/head.php"); ?>
+        <title>Rubicon Outdoor Centre - Rubicon Campus - Students</title>
+    </head>
+    <body id="top" class="page--students">
+        <?php include("/includes/header.php"); ?>
+        <div class="main-content  grid">
+            <div class="page-links  grid__col--33">
+                <ul class="page-links__list">
+                    <li><a class="page-links__anchor" href="#forms_and_information">Forms and Information</a></li>
+                    <li><a class="page-links__anchor" href="#code_of_conduct">Code of Conduct</a></li>
+                    <li><a class="page-links__anchor" href="#work_experience">Work Experience</a></li>
+                </ul>
+            </div>
+            <div class="grid__col--66">
+                <section id="forms_and_information" class="section">
+                    <h2 class="section__title">Forms and Information</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <h3>Student Forms</h3>
+                    <ul class="form-list">
+                        <li class="form-list__list-item  form-list__list-item--attention">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Student Medical and Consent Form</h6>
+                            <p class="form-list__list-item__description">The Rubicon Student Medical and Consent Form <u>must</u> be used. Please send/fax 2 weeks prior to the Program.</p>
+                        </li>
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Horse Riding Permission Form</h6>
+                            <p class="form-list__list-item__description">This form must be completed by staff (who choose to ride) and all students if your program involves Horse Riding.</p>
+                        </li>
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Student Equipment List</h6>
+                            <p class="form-list__list-item__description">Please download the list and give to the students who are attending the camp.</p>
+                        </li>
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Overnight Equipment List</h6>
+                            <p class="form-list__list-item__description">Please download the list and give to the students who are going on an overnight campout.</p>
+                        </li>
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Asthma Management Plan</h6>
+                            <p class="form-list__list-item__description">If applicable, must be attached to Medical Form.</p>
+                        </li>
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Action Plan for Allergic Reactions</h6>
+                            <p class="form-list__list-item__description">If applicable, must be attached to Medical Form.</p>
+                        </li>
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <a class="form-list__anchor  form-list__anchor--doc" href="#"></a>
+                            <h6 class="form-list__list-item__title">Action Plan for Anaphylaxis</h6>
+                            <p class="form-list__list-item__description">If applicable, must be attached to Medical Form.</p>
+                        </li>
+                    </ul>
+                    <hr>
+                    <p><em>If you have trouble viewing these files please download the latest version of <a href="http://www.adobe.com/products/acrobat/readstep2.php">Acrobat Reader</a>.</em></p>
+                </section>
+                <section id="code_of_conduct" class="section">
+                    <h2 class="section__title">Code of Conduct</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <h3>Guiding Principles</h3>
+                    <ul>
+                        <li><p>To ensure that the rights of all individuals are protected whilst at the Centre, and travelling to and from the Centre.</p></li>
+                        <li><p>To create the best possible learning environment.</p></li>
+                        <li><p>To ensure that breaches of our code of conduct are treated in a fair and consistent manner and within the expectations of staff and students.</p></li>
+                    </ul>
+                    <h3>Rights</h3>
+                    <ul>
+                        <li><p>All individuals and their property are to be valued and treated with <u>respect</u>.</p></li>
+                        <li><p>All individuals have the right to a safe, secure and cooperative working environment in which participation, risk taking and confronting challenges are encouraged.</p></li>
+                        <li><p>All individuals have the right to privacy.</p></li>
+                        <li><p>Staff should be able to perform their duties in an atmosphere of order and with cooperation.</p></li>
+                        <li><p>Staff, students and the natural environment have the right to exist in a healthy and environmentally considerate atmosphere.</p></li>
+                        <li><p>The Centre expects support from all students, parents, and staff in implementing this Code of Conduct.</p></li>
+                    </ul>
+                    <h3>Responsibilities</h3>
+                    <ul>
+                        <li><p>All students should treat one another, staff property and the environment, with <u>respect</u>.</p></li>
+                        <li><p>All students should act and behave in a way which does not endanger, intimidate or interfere with the participation of others.</p></li>
+                        <li><p>All students should respect others rights to privacy.</p></li>
+                        <li><p>Students should comply with instructions given by any members of staff and should willingly contribute to the daily routine of the Centre.</p></li>
+                        <li><p>All students should conduct themselves in a safe, healthy and environmentally friendly manner.</p></li>
+                        <li><p>All students should behave according to this code and accept the consequences if it is breached. All staff should fairly, reasonably and consistently implement this code. Staff and parents of participating schools should support the Centre in implementing the Code of Conduct.</p></li>
+                    </ul>
+                    <h3>Rubicon Expectations</h3>
+                    <ol>
+                        <li><p>Students will comply with the directions of Rubicon Staff.</p></li>
+                        <li><p>Fighting, bullying and any other form of aggressive behaviour, including offensive, sexist or racist language, will be seen as a breech of our Code of Conduct.</p></li>
+                        <li><p>Students are requested to <u>not</u> leave the Centre, unless they have the permission of the Principal or the Rubicon Teacher on duty.  For reasons of safety, certain areas are out of bounds to all students; these will be outlined upon arrival.</p></li>
+                        <li><p>Cigarettes, alcohol, illicit drugs, offensive weapons, and pornographic material must not be brought to the Centre.</p></li>
+                        <li><p>Rubicon recommends that personal listening devices and mobile phones are not consistent with the aims of our program. If brought to the Centre, they must not to be used during activity time. The Centre takes no responsibility for any loss or damage to these items.</p></li>
+                        <li><p>Students will dress appropriately at all times, including following Sun Smart requirements during Terms 1 and 4; this includes wearing hats and no singlet tops.</p></li>
+                    </ol>
+                </section>
+                <section id="work_experience" class="section">
+                    <h2 class="section__title">Work Experience</h2>
+                    <a class="section__back-to-top" href="#top"><i class="icon-font  icon-font--up-arrow"></i></a>
+                    <ul class="form-list">
+                        <li class="form-list__list-item">
+                            <a class="form-list__anchor  form-list__anchor--pdf" href="#"></a>
+                            <h6 class="form-list__list-item__title  form-list__list-item__title--single">Work Experience Application</h6>
+                            <p class="form-list__list-item__description  form-list__list-item__description--single">For Secondary Students applying for Work Experience</p>
+                        </li>
+                    </ul>
+                    <hr>
+                    <p>To be eligible for Work Experience at Rubicon, students must be in Year 10 or above. Successful applicants are assigned to a qualified teacher to work with duing the placement. Work experience students assist the Rubicon staff in providing a range of outdoor adventure activities and general duties whilst on site.</p>
+                    <p>Accommodation and meals are provided for the duration of the experience; a fee is charged for meals. Further information will be provided to successful applicants.</p>
+                    <p>Please download an Application form, complete it, then send to the Rubicon Campus (details on the form).</p>
+                </section>
+            </div>
+        </div>
+<?php include("/includes/footer.php"); ?>
