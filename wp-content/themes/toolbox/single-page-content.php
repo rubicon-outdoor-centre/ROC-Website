76530ebe96347bd2a0dc984aea88741c3d6319cc
Joshua Hibbert <joshuanhibbert@gmail.com>
2013-02-25 16:30:47 +1100

    update single pages

---
+++ wp-content/themes/toolbox/single-page-content.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package Toolbox
+ * @since Toolbox 1.0
+ */
+?>
+
+<section>
+	<?php the_content(); ?>
+</section
--- wp-content/themes/toolbox/single-page.php
+++ wp-content/themes/toolbox/single-page.php
@@ -1,11 +1,7 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * Template Name: Single Page
+ * Description: A full-width template with no sidebar
  *
  * @package Toolbox
  * @since Toolbox 0.1
@@ -18,7 +14,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+					<?php get_template_part( 'single-page-content' ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
