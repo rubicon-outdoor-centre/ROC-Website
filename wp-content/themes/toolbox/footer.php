d62ec9649f97a26afb33214ba701b91f7bc5a67c
Joshua Hibbert <joshuanhibbert@gmail.com>
2013-02-27 15:35:12 +1100

    update navigation

--- wp-content/themes/toolbox/footer.php
+++ wp-content/themes/toolbox/footer.php
@@ -4,7 +4,22 @@
 
 		<footer class="footer" role="contentinfo">
 
-			<p>© <?php echo date('Y') ?> <?php bloginfo('name'); ?></p>
+			<p class="copyright">© <?php echo date('Y') ?> <?php bloginfo('name'); ?></p>
+
+            <nav class="primary-nav" role="navigation">
+                <?php
+
+                    $args = array(
+                                'theme_location' => 'primary',
+                                'container' => false,
+                                'items_wrap' => '<ul>%3$s</ul>'
+                            );
+
+                    wp_nav_menu( $args );
+
+                ?>
+                <a class="back-to-top" href="#top">&uarr;</a>
+            </nav>
 
 		</footer>
 
