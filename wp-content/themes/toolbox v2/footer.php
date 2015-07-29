3d25bdef7a618f53a7c3b236147e113f0d4a9654
Joshua Hibbert <joshuanhibbert@gmail.com>
2015-07-29 13:53:06 +1000

    add google analytics

--- wp-content/themes/toolbox v2/footer.php
+++ wp-content/themes/toolbox v2/footer.php
@@ -65,6 +65,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE. */
 </script>
 
+<!-- Google Analytics Code -->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-18073514-1', 'auto');
+	ga('send', 'pageview');
+</script>
+
 </body>
 
 </html>
